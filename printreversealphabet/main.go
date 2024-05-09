b346006e82aaa43dc064885e5e568e18eedf456a
smiddleto <sean@middleton.fi>
2024-05-10 02:35:55 +0300

    This is a for loop using a range on a string

---
+++ printreversealphabet/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	// **** alphabet forwards ***
+	// alphabet := "abcdefghijklmnopqrstuvwxyz"
+
+	// *** alphabet backwards ***
+	// a so called "raw character literal string" 
+	// alphabet := `zyxwvutsrqponmlkjihgfedcba`
+	alphabet := "zyxwvutsrqponmlkjihgfedcba"
+
+	for _, value := range alphabet {
+		z01.PrintRune(value)
+	}
+
+	z01.PrintRune('\n')
+}
