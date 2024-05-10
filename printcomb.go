33494d688ff573e66059595b3829ed4c48ce75c5
smiddleto <sean@middleton.fi>
2024-05-10 18:28:07 +0300

    equalities etc == with <=

--- printcomb.go
+++ printcomb.go
@@ -9,12 +9,12 @@ func PrintComb() {
 	// 3 chars long
 	for i := '0'; i <= '7'; i++ {
 		for j := '1'; j <= '8'; j++ {
-			for k := '0'; k <= '9'; k++ {
-				if i < j || j < k {
+			for k := '2'; k <= '9'; k++ {
+				if i < j || j < k || i < k{
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(k)
-					if !(i == 7 || j == 8 || j == 9) {
+					if !(i == '7' || j == '8' || k == '9') {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
