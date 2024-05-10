08e209adc597b272d2fca33c2111c7632edabdbe
smiddleto <sean@middleton.fi>
2024-05-10 18:46:05 +0300

    finally got the and right as &&

--- printcomb.go
+++ printcomb.go
@@ -10,11 +10,11 @@ func PrintComb() {
 	for i := '0'; i <= '7'; i++ {
 		for j := '1'; j <= '8'; j++ {
 			for k := '2'; k <= '9'; k++ {
-				if ((i < j) || (j < k) || (i < k)) {
+				if ((i < j) && (j < k) && (i < k)) {
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(k)
-					if (i != '7' || j != '8' || k != '9') {
+					if (i != '7' && j != '8' && k != '9') {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
