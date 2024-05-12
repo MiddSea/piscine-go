33e7ab9fb36112a7570fe66508f4eee0b7957058
smiddleto <sean@middleton.fi>
2024-05-12 18:34:44 +0300

    bottom line do

--- quadA.go
+++ quadA.go
@@ -6,19 +6,31 @@ func QuadA(x, y int) {
 	fmt.Println("x, y QuadA")
 
 	//top line
-	if x >= 1 && y >= 1 {
+	if x >= 1 && y >= 1 { // x min 1 o
 		print("o") // top left corner
-		if x > 2 {
+		if x > 2 { // x min 3 o-o for dashes
 			for width := 2; width < x; width++ {
-				print("-") // top line
+				print("-") // '-' dashes top line
 			}
 		}
-		if x > 3 {
-			print("o") // last
+		if x > 1 { // x min 2: oo
+			print("o") // last 'o' top line
 		}
 	}
 
 	//lines according to height
 
-	//last line
+	//bottom line
+	if x >= 1 && y >= 2 { // x min 1 o
+		print("o") // bottom left corner
+		if x > 2 { // x min 3 o-o for dashes
+			for width := 2; width < x; width++ {
+				print("-") // '-' dashes bottom line
+			}
+		}
+		if x > 1 { // x min 2: oo
+			print("o") // last 'o' bottom line
+		}
+	}
+
 }
