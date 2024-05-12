93f66bc60002241fa511cfcaf0cdd5647e847b4b
smiddleto <sean@middleton.fi>
2024-05-12 18:14:16 +0300

    using x instead of width

--- quadA.go
+++ quadA.go
@@ -6,18 +6,17 @@ func QuadA(x, y int) {
 	fmt.Println("x, y QuadA")
 
 	//top line
-		if x >= 1 && y >= 1 {
-			print("o")  // top left corner
-		if width > 3 {
-			for width := 2 width < x+1; width++ {
-				print("-") // top line 
+	if x >= 1 && y >= 1 {
+		print("o") // top left corner
+		if x > 3 {
+			for width := 2; width < x+1; width++ {
+				print("-") // top line
 			}
-		} 
+		}
 		print("o") // last
 	}
-	
-	
-	//lines according to height 
+
+	//lines according to height
 
 	//last line
 }
