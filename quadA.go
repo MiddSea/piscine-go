04cd88d032071936cb631d082bcc899d1fc6970f
smiddleto <sean@middleton.fi>
2024-05-12 18:07:06 +0300

    top line of quadA func

--- quadA.go
+++ quadA.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func QuadA(x, y int) {
 	fmt.Println("x, y QuadA")
+
+	//top line
+		if x >= 1 && y >= 1 {
+			print("o")  // top left corner
+		if width > 3 {
+			for width :=  width < x+1; width++ {
+				print("-") // top line 
+			}
+		} 
+		print("o") // last
+	}
+	
+	
+	//lines according to height 
+
+	//last line
 }
