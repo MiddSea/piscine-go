5ffdbaaa0b86e1b678bc582b5d90f97ae9aaccca
smiddleto <sean@middleton.fi>
2024-05-12 17:39:41 +0300

    here is QuadA prog 1

---
+++ quadA-test/quadA-test-prog1.go
@@ -0,0 +1,16 @@
+package main
+
+import "piscine"
+
+func main() {
+	piscine.QuadA(5, 3)
+}
+
+// Output:
+// And its output :
+
+// $ go run .
+// o---o
+// |   |
+// o---o
+// $
---
+++ quadA.go
@@ -0,0 +1,7 @@
+package piscine
+
+import "fmt"
+
+func QuadA(x, y int) {
+	fmt.Println("x, y QuadA")
+}
