37082b542668db52e4e59ff8f374d8c5539e1bc9
smiddleto <sean@middleton.fi>
2024-05-13 19:38:51 +0300

    QuadA modified a bit

--- quadA.go
+++ quadA.go
@@ -1,22 +1,23 @@
 package piscine
 
 import (
-	"fmt"
+	zz "github.com/01-edu/z01"
 )
 
 func QuadA(x, y int) {
-	fmt.Println("x, y QuadA")
+	// fmt.Prixxxntlnxxx("x, y QuadA")
 
 	// top line
 	if x >= 1 && y >= 1 { // x min 1 o
-		print("o") // top left corner
-		if x > 2 { // x min 3 o-o for dashes
+		zz.PrintRune('o') // top left corner
+		if x > 2 {        // x min 3 o-o for dashes
 			for width := 2; width < x; width++ {
-				print("-") // '-' dashes top  for the top line
+				zz.PrintRune('-') // '-' dashes top  for the top line
 			}
 		}
 		if x > 1 { // x min 2: oo
-			print("o\n") // last 'o' top line
+			zz.PrintRune('o')  // last 'o' top line
+			zz.PrintRune('\n') // last 'o' top line
 		}
 	}
 
@@ -24,14 +25,14 @@ func QuadA(x, y int) {
 	if y > 2 {
 		for height := 2; height < y; height++ {
 			if x >= 1 && y >= 2 { // x min 1 o
-				print("|") // left column
-				if x > 2 { // x min 3 | | for spaces
+				zz.PrintRune('|') // left column
+				if x > 2 {        // x min 3 | | for spaces
 					for width := 2; width < x; width++ {
-						print(" ") // ' ' space in lines
+						zz.PrintRune(' ') // ' ' space in lines
 					}
 				}
 				if x > 1 { // x min 2: ||
-					print("|\n") // last '|' in line
+					zz.PrintRune('|') // last '|' in line
 				}
 			}
 		}
@@ -39,14 +40,16 @@ func QuadA(x, y int) {
 
 	// bottom line
 	if x >= 1 && y >= 2 { // x min 1 o
-		print("o") // bottom left corner
-		if x > 2 { // x min 3 o-o for dashes
+		zz.PrintRune('o') // bottom left corner
+		if x > 2 {        // x min 3 o-o for dashes
 			for width := 2; width < x; width++ {
-				print("-") // '-' dashes bottom line
+				zz.PrintRune('-') // '-' dashes bottom line
 			}
 		}
 		if x > 1 { // x min 2: oo
-			print("o\n") // last 'o' bottom line
+			zz.PrintRune('o')  // last 'o' bottom line
+			zz.PrintRune('\n') // last 'o' bottom line
+
 		}
 	}
 }
