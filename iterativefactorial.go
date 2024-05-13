9a3285d9aa3efe36a09ee273f653910d1d1ee4eb
smiddleto <sean@middleton.fi>
2024-05-13 20:18:56 +0300

    new routine with b

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,16 +1,31 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	a := 1
-	if nb == 1 || nb == 0 {
-		return 1
-	} else if nb > 1 && nb <= 25 {
-		for i := 1; i <= nb; i++ {
-			a = a * i
-		}
-	} else {
-		return 0
-	}
+	// 	a := 1
+	// 	if nb == 1 || nb == 0 {
+	// 		return 1
+	// 	} else if nb > 1 && nb <= 25 {
+	// 		for i := 1; i <= nb; i++ {
+	// 			a = a * i
+	// 		}
+	// 	} else {
+	// 		return 0
+	// 	}
 
-	return a
+	// 	return a
+	// }
+	if nb < 25 {
+		if nb == 0 {
+			return 1
+		}
+		if nb < 0 {
+			return 0
+		}
+		var b = 1
+		for a := nb; a >= 1; a-- {
+			b *= a
+		}
+		return b
+	}
+	return 0
 }
