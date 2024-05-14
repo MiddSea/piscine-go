73543ea59db9771d346001453bf6f0e359edff98
smiddleto <sean@middleton.fi>
2024-05-14 21:14:33 +0300

    4ubak's version

--- findnextprime.go
+++ findnextprime.go
@@ -1,12 +1,28 @@
 package piscine
 
+// func FindNextPrime(nb int) int {
+// 	ret := nb
+// 	if ret <= 0 {
+// 		ret = 2
+// 	}
+// 	for !IsPrime(ret) {
+// 		ret++
+// 	}
+// 	return ret
+// }
+
+
+
+// https://github.com/4ubak/piscine-go/blob/master/findnextprime.go 
+// frm oct 2019
+// 4ubak 4ubak Update findnextprime.go
+
+// 
 func FindNextPrime(nb int) int {
-	ret := nb
-	if ret <= 0 {
-		ret = 2
+	for a := nb; a >= nb; a++ {
+		if IsPrime(a) {
+			return a
+		}
 	}
-	for !IsPrime(ret) {
-		ret++
-	}
-	return ret
+	return 1
 }
