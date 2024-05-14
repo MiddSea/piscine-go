d292053f965f168143b60c935f717f42e728c0fd
smiddleto <sean@middleton.fi>
2024-05-14 06:52:53 +0300

    copied and done

---
+++ iterativepower.go
@@ -0,0 +1,13 @@
+package piscine
+
+// q04 excersoze
+func IterativePower(nb, power int) int {
+	if power < 0 {
+		return 0
+	}
+	ret := 1
+	for ; power > 0; power-- {
+		ret *= nb
+	}
+	return ret
+}
---
+++ q04.iterativepower/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+
+func main() {
+	fmt.Println(piscine.IterativePower(4, 3))
+}
+// And its output :
+
+// $ go run .
+// 64
+// $
