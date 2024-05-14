87a4896f0387aa434f49e2dcded59ff44a727070
smiddleto <sean@middleton.fi>
2024-05-14 21:19:06 +0300

    find the next prime (see findnesxtprime copy.go.bak)

--- findnextprime.go
+++ findnextprime.go
@@ -1,23 +1,6 @@
 package piscine
 
-// func FindNextPrime(nb int) int {
-// 	ret := nb
-// 	if ret <= 0 {
-// 		ret = 2
-// 	}
-// 	for !IsPrime(ret) {
-// 		ret++
-// 	}
-// 	return ret
-// }
-
-
-
-// https://github.com/4ubak/piscine-go/blob/master/findnextprime.go 
-// frm oct 2019
-// 4ubak 4ubak Update findnextprime.go
-
-// 
+// finds the next prime
 func FindNextPrime(nb int) int {
 	for a := nb; a >= nb; a++ {
 		if IsPrime(a) {
