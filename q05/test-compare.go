f9380e7cda7d3ad5bfb89de82a49f44233ca0793
smiddleto <sean@middleton.fi>
2024-05-15 09:46:56 +0300

    05 compare

---
+++ q05/test-compare.go
@@ -0,0 +1,21 @@
+
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.Compare("Hello!", "Hello!"))
+	fmt.Println(piscine.Compare("Salut!", "lut!"))
+	fmt.Println(piscine.Compare("Ola!", "Ol"))
+}
+
+//And its output :
+
+// $ go run .
+// 0
+// -1
+// 1
+// $
