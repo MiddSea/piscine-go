af794f286530c0e9939cbb7b92d4169562e81363
sean@middleton.fi <smiddleto>
2024-05-15 19:52:32 +0300

    is pintable and tested...

---
+++ q05/test-isPrintable.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.IsPrintable("abc\n"))
+	fmt.Println(piscine.IsPrintable("a\bc\a"))
+	fmt.Println(piscine.IsPrintable("ab\tc"))
+	fmt.Println(piscine.IsPrintable("adfsa	sdf asdsfa asdfdsa "))
+	fmt.Println(piscine.IsPrintable("a.,.,. eadsf a.-., /\\"))
+	fmt.Println(piscine.IsPrintable("a true "))
+}
