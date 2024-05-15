6868eb629714d0598902e0c47335365cf9d2d009
smiddleto <sean@middleton.fi>
2024-05-15 12:02:10 +0300

    this is concat

---
+++ q05/test-concat.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.Concat("Hello!", " How are you?"))
+
+}
+
+// And its output :
+
+// $ go run .
+// Hello! How are you?
+// $
