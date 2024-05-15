99746473153de02161656f642724eb2c48bf0ab5
sean@middleton.fi <smiddleto>
2024-05-15 19:54:22 +0300

    is pintable and tested...

--- isprintable.go
+++ isprintable.go
@@ -1,6 +1,5 @@
 package piscine
 
-
 func IsPrintable(s string) bool {
 		for _, r := range s {
 			if !(r >= 32 && r < 127 ) {
@@ -9,5 +8,3 @@ func IsPrintable(s string) bool {
 		}
 		return true
 	}
-
-
