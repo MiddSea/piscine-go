9a57423d5403ac36326015c295e8409776d743cd
sean@middleton.fi <smiddleto>
2024-05-15 13:26:33 +0300

    changed checking logic to if !(('a' >= s[i] && s[i] <= 'z') || ('A' >= s[i] && s[i] <= 'Z')) {

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for i := 0; i < len(s); i++ {
-		if !(('a' <= s[i] && s[i] <= 'z') || ('A' <= s[i] && s[i] <= 'Z')) {
+		if !(('a' >= s[i] && s[i] <= 'z') || ('A' >= s[i] && s[i] <= 'Z')) {
 			return false
 		}
 	}
