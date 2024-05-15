c606376ee8fae288ea2e56d995a234a039871c11
smiddleto <sean@middleton.fi>
2024-05-15 11:20:18 +0300

    index ready for action

---
+++ index.go
@@ -0,0 +1,17 @@
+package piscine
+
+// q05 string.
+
+func Index(s, toFind string) int {
+	lenS := len(s)
+	lenF := len(toFind)
+	for i := range s {
+		if i+lenF > lenS {
+			return -1
+		}
+		if s[i:i+lenF] == toFind {
+			return i
+		}
+	}
+	return -1
+}
---
+++ q05/test-index.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.Index("Hello!", "l"))
+	fmt.Println(piscine.Index("Salut!", "alu"))
+	fmt.Println(piscine.Index("Ola!", "hOl"))
+}
+
+// from q05 index
+// And its output :
+
+// $ go run .
+// 2
+// 1
+// -1
+// $
+
+// import (
+// 	"fmt"
+// 	"strings"
+// )
+
+// func main() {
+// 	fmt.Println(strings.Index("chandler", "and"))
+// 	fmt.Println(strings.Index("navy", "as"))
+// }
