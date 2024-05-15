60ee4cfb8e458d8725d2a190ae50d0ef59a4f8fb
sean@middleton.fi <smiddleto>
2024-05-15 23:25:10 +0300

    to upper test bak To Upper started

---
+++ q05/test-ToLower.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.ToUpper("abcABA d d d ... .[[87/||\\]]"))
+	fmt.Println(piscine.ToUpper("abcABA d d d ... .[[87/||\\]]"))
+	fmt.Println(piscine.ToUpper("abcABA d243fn fe  d d ... .[[87/||\\]]"))
+	fmt.Println(piscine.ToUpper("adfsa	s243fn fe df asdsfa asdfdsa "))
+	fmt.Println(piscine.ToUpper("a.,.,. e243fn fe adsf a.-., /\\"))
+	fmt.Println(piscine.ToUpper("a true "))
+}
+
+// testing to upper done works 2024-05-15
