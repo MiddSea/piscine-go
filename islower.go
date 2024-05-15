11ef497b5b64e0cabedc7a1dc9ac837f92510aba
sean@middleton.fi <smiddleto>
2024-05-15 13:15:47 +0300

    is lower as check for upper A - Z check if true - return false.

--- islower.go
+++ islower.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsLower(s string) bool {
 	for i := 0; i < len(s); i++ {
-		if !(('a' <= s[i] && s[i] <= 'z') || ('A' <= s[i] && s[i] <= 'Z')) {
+		if 'A' <= s[i] && s[i] <= 'Z' {
 			return false
 		}
 	}
