2d2f8b5d36a330225593df834e955f48437a12d8
sean@middleton.fi <smiddleto>
2024-05-15 21:27:12 +0300

    bit of a mess but fun anyway!!

---
+++ toupper.go
@@ -0,0 +1,12 @@
+package piscine
+
+func ToUpper(s string) string {
+	x map[rune]rune { 'a' : 'A', 'b' : 'B', 'c' : 'C', 'd' : 'D', 'e' : 'E', 'f' : 'F', 'g' : 'G', 'h' : 'H', 'i' : 'I', 'j' : 'J', 'k' : 'K', 'l' : 'L', 'm' : 'M', 'n' : 'N', 'o' : 'O', 'p' : 'P', 'q' : 'Q', 'r' : 'R', 's' : 'S', 't' : 'T', 'u' : 'U', 'v' : 'V', 'w' : 'W', 'x' : 'X', 'y' : 'Y', 'z' : 'Z',  }
+	for q2awz< {
+		if !('A' <= s[i] && s[i] <= 'Z') {
+			return false
+		}
+	} 
+	return true 
+}
+// this is Blah does fkdj vdlF 
