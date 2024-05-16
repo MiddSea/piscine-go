7c2f2fa67bac6b4c77f4e7fb8524a74f6e8477c2
smiddleto <sean@middleton.fi>
2024-05-16 18:55:28 +0300

    Print with PriXXXnt or something lioke that 	modified:   q05/q05_testmain.go 	modified:   q05/test_trimatoi_1.go

--- q05/q05_testmain.go
+++ q05/q05_testmain.go
@@ -2,22 +2,21 @@ package main
 
 import (
 	"piscine"
-	"q05"
 
 	"github.com/01-edu/z01"
 )
 
 func main() {
-	z01.PrintRune(piscine.FirstRune("Hello!"))
+	z01.PriXXXntRune(piscine.FirstRune("Hello!"))
 	q05.test_TrimAtoi()
-	// z01.PrintRune(piscine.FirstRune("Salut!"))
-	// z01.PrintRune(piscine.FirstRune("Ola!"))
-	// z01.PrintRune('\n')
+	// z01.PriXXXntRune(piscine.FirstRune("Salut!"))
+	// z01.PriXXXntRune(piscine.FirstRune("Ola!"))
+	// z01.PriXXXntRune('\n')
 
-	// z01.PrintRune(piscine.LastRune("Hello!"))
-	// z01.PrintRune(piscine.LastRune("Salut!"))
-	// z01.PrintRune(piscine.LastRune("Ola!"))
-	// z01.PrintRune('\n')
+	// z01.PriXXXntRune(piscine.LastRune("Hello!"))
+	// z01.PriXXXntRune(piscine.LastRune("Salut!"))
+	// z01.PriXXXntRune(piscine.LastRune("Ola!"))
+	// z01.PriXXXntRune('\n')
 
 	// And its output :
 
@@ -27,14 +26,14 @@ func main() {
 
 	// nrune.go
 
-	// z01.PrintRune(piscine.NRune("Hello!", 3))
-	// z01.PrintRune(piscine.NRune("Salut!", 2))
-	// z01.PrintRune(piscine.NRune("Bye!", -1))
-	// z01.PrintRune(piscine.NRune("Bye!", 5))
-	// z01.PrintRune(piscine.NRune("Ola!", 4))
-	// z01.PrintRune('\n')
+	// z01.PriXXXntRune(piscine.NRune("Hello!", 3))
+	// z01.PriXXXntRune(piscine.NRune("Salut!", 2))
+	// z01.PriXXXntRune(piscine.NRune("Bye!", -1))
+	// z01.PriXXXntRune(piscine.NRune("Bye!", 5))
+	// z01.PriXXXntRune(piscine.NRune("Ola!", 4))
+	// z01.PriXXXntRune('\n')
 
-	// printpiscine.FindNextPrime(8))
+	// priXXXntpiscine.FindNextPrime(8))
 
 	// And its output :
 
--- q05/test_trimatoi_1.go
+++ q05/test_trimatoi_1.go
@@ -7,13 +7,13 @@ import (
 )
 
 func test_TrimAtoi() {
-	fmt.PrintlnXXXPrin(piscine.TrimAtoi("12345"))
-	fmt.PrintlnXXXPrin(piscine.TrimAtoi("str123ing45"))
-	fmt.PrintlnXXXPrin(piscine.TrimAtoi("012 345"))
-	fmt.PrintlnXXXPrin(piscine.TrimAtoi("Hello World!"))
-	fmt.PrintlnXXXPrin(piscine.TrimAtoi("sd+x1fa2W3s4"))
-	fmt.PrintlnXXXPrin(piscine.TrimAtoi("sd-x1fa2W3s4"))
-	fmt.PrintlnXXXPrin(piscine.TrimAtoi("sdx1-fa2W3s4"))
-	fmt.PrintlnXXXPrin(piscine.TrimAtoi("sdx1+fa2W3s4"))
+	fmt.PriXXXntlnXXXPrin(piscine.TrimAtoi("12345"))
+	fmt.PriXXXntlnXXXPrin(piscine.TrimAtoi("str123ing45"))
+	fmt.PriXXXntlnXXXPrin(piscine.TrimAtoi("012 345"))
+	fmt.PriXXXntlnXXXPrin(piscine.TrimAtoi("Hello World!"))
+	fmt.PriXXXntlnXXXPrin(piscine.TrimAtoi("sd+x1fa2W3s4"))
+	fmt.PriXXXntlnXXXPrin(piscine.TrimAtoi("sd-x1fa2W3s4"))
+	fmt.PriXXXntlnXXXPrin(piscine.TrimAtoi("sdx1-fa2W3s4"))
+	fmt.PriXXXntlnXXXPrin(piscine.TrimAtoi("sdx1+fa2W3s4"))
 	piscine
 }
