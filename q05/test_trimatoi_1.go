61a8e99925d20d97a5b21bcc1369f1d3803054cf
smiddleto <sean@middleton.fi>
2024-05-16 18:53:35 +0300

    replaced println with PrintXXXXln

--- q05/test_trimatoi_1.go
+++ q05/test_trimatoi_1.go
@@ -7,13 +7,13 @@ import (
 )
 
 func test_TrimAtoi() {
-	fmt.Println(piscine.TrimAtoi("12345"))
-	fmt.Println(piscine.TrimAtoi("str123ing45"))
-	fmt.Println(piscine.TrimAtoi("012 345"))
-	fmt.Println(piscine.TrimAtoi("Hello World!"))
-	fmt.Println(piscine.TrimAtoi("sd+x1fa2W3s4"))
-	fmt.Println(piscine.TrimAtoi("sd-x1fa2W3s4"))
-	fmt.Println(piscine.TrimAtoi("sdx1-fa2W3s4"))
-	fmt.Println(piscine.TrimAtoi("sdx1+fa2W3s4"))
+	fmt.PrintlnXXXPrin(piscine.TrimAtoi("12345"))
+	fmt.PrintlnXXXPrin(piscine.TrimAtoi("str123ing45"))
+	fmt.PrintlnXXXPrin(piscine.TrimAtoi("012 345"))
+	fmt.PrintlnXXXPrin(piscine.TrimAtoi("Hello World!"))
+	fmt.PrintlnXXXPrin(piscine.TrimAtoi("sd+x1fa2W3s4"))
+	fmt.PrintlnXXXPrin(piscine.TrimAtoi("sd-x1fa2W3s4"))
+	fmt.PrintlnXXXPrin(piscine.TrimAtoi("sdx1-fa2W3s4"))
+	fmt.PrintlnXXXPrin(piscine.TrimAtoi("sdx1+fa2W3s4"))
 	piscine
 }
