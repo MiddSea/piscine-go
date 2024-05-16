b7cdaa585962d0cda6998ae1267a6120686acdf6
smiddleto <sean@middleton.fi>
2024-05-16 19:32:22 +0300

    to upper and lower

--- tolower.go
+++ tolower.go
@@ -1,17 +1,30 @@
 package piscine
 
 func ToLower(s string) string {
-	x := map[rune]rune{'A': 'a', 'B': 'b', 'C': 'c', 'D': 'd', 'E': 'e', 'F': 'f', 'G': 'g', 'H': 'h', 'I': 'i', 'J': 'j', 'K': 'k', 'L': 'l', 'M': 'm', 'N': 'n', 'O': 'o', 'P': 'p', 'Q': 'q', 'R': 'r', 'S': 's', 'T': 't', 'U': 'u', 'V': 'v', 'W': 'w', 'X': 'x', 'Y': 'y', 'Z': 'z'}
-	var result []rune
-	for _, r := range s {
-		lower, ok := x[r]
-		if ok {
-			result = append(result, lower)
+	var newString string
+	for i := 0; i < len(s); i++ {
+		if 'A' <= s[i] && s[i] <= 'Z' {
+			newString += string(s[i] - 'A' + 'a')
 		} else {
-			result = append(result, r)
+			newString += string(s[i])
 		}
 	}
-	return string(result)
+	return newString
 }
+
+// anclarma-rep
+// func ToLower(s string) string {
+// 	x := map[rune]rune{'A': 'a', 'B': 'b', 'C': 'c', 'D': 'd', 'E': 'e', 'F': 'f', 'G': 'g', 'H': 'h', 'I': 'i', 'J': 'j', 'K': 'k', 'L': 'l', 'M': 'm', 'N': 'n', 'O': 'o', 'P': 'p', 'Q': 'q', 'R': 'r', 'S': 's', 'T': 't', 'U': 'u', 'V': 'v', 'W': 'w', 'X': 'x', 'Y': 'y', 'Z': 'z'}
+// 	var result []rune
+// 	for _, r := range s {
+// 		lower, ok := x[r]
+// 		if ok {
+// 			result = append(result, lower)
+// 		} else {
+// 			result = append(result, r)
+// 		}
+// 	}
+// 	return string(result)
+// }
 
 // this is the reversed map for toLower
--- toupper.go
+++ toupper.go
@@ -1,17 +1,31 @@
 package piscine
 
 func ToUpper(s string) string {
-	x := map[rune]rune{'a': 'A', 'b': 'B', 'c': 'C', 'd': 'D', 'e': 'E', 'f': 'F', 'g': 'G', 'h': 'H', 'i': 'I', 'j': 'J', 'k': 'K', 'l': 'L', 'm': 'M', 'n': 'N', 'o': 'O', 'p': 'P', 'q': 'Q', 'r': 'R', 's': 'S', 't': 'T', 'u': 'U', 'v': 'V', 'w': 'W', 'x': 'X', 'y': 'Y', 'z': 'Z'}
-	var result []rune
-	for _, r := range s {
-		big, ok := x[r]
-		if ok {
-			result = append(result, big)
+	var newString string
+	for i := 0; i < len(s); i++ {
+		if 'a' <= s[i] && s[i] <= 'z' {
+			newString += string(s[i] - 'a' + 'A')
 		} else {
-			result = append(result, r)
+			newString += string(s[i])
 		}
 	}
-	return string(result)
+	return newString
 }
 
-// this is the old one for ToUpper
+// package piscine
+
+// func ToUpper(s string) string {
+// 	x := map[rune]rune{'a': 'A', 'b': 'B', 'c': 'C', 'd': 'D', 'e': 'E', 'f': 'F', 'g': 'G', 'h': 'H', 'i': 'I', 'j': 'J', 'k': 'K', 'l': 'L', 'm': 'M', 'n': 'N', 'o': 'O', 'p': 'P', 'q': 'Q', 'r': 'R', 's': 'S', 't': 'T', 'u': 'U', 'v': 'V', 'w': 'W', 'x': 'X', 'y': 'Y', 'z': 'Z'}
+// 	var result []rune
+// 	for _, r := range s {
+// 		big, ok := x[r]
+// 		if ok {
+// 			result = append(result, big)
+// 		} else {
+// 			result = append(result, r)
+// 		}
+// 	}
+// 	return string(result)
+// }
+
+// // this is the old one for ToUpper
