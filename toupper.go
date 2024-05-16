97053af46a85e4e3a14f1107338a84b9e6d96187
smiddleto <sean@middleton.fi>
2024-05-16 22:33:44 +0300

    package add

--- toupper.go
+++ toupper.go
@@ -1,3 +1,4 @@
+package piscine
 
 func ToUpper(s string) string {
 	x := map[rune]rune{'a': 'A', 'b': 'B', 'c': 'C', 'd': 'D', 'e': 'E', 'f': 'F', 'g': 'G', 'h': 'H', 'i': 'I', 'j': 'J', 'k': 'K', 'l': 'L', 'm': 'M', 'n': 'N', 'o': 'O', 'p': 'P', 'q': 'Q', 'r': 'R', 's': 'S', 't': 'T', 'u': 'U', 'v': 'V', 'w': 'W', 'x': 'X', 'y': 'Y', 'z': 'Z'}
