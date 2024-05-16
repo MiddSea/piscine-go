2f166d3d1a42c4bcc80babb0b7683eb9c6f9b58d
smiddleto <sean@middleton.fi>
2024-05-16 19:00:00 +0300

    starting with these ones new file:   capitalize.go renamed:    toupper.go.bak -> toupper.go

---
+++ capitalize.go
@@ -0,0 +1,21 @@
+package piscine
+
+// induced and Capital
+func Capitalize(s string) string {
+	var newString string
+	isFirst := true
+	for i := 0; i < len(s); i++ {
+		if IsAlpha(string(s[i])) {
+			if isFirst {
+				newString += ToUpper(string(s[i]))
+			} else {
+				newString += ToLower(string(s[i]))
+			}
+			isFirst = false
+		} else {
+			newString += string(s[i])
+			isFirst = true
+		}
+	}
+	return newString
+}
