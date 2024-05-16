ac8a3ce1fe6a5f6b2e5e6a394bc3697ba1a15d9d
smiddleto <sean@middleton.fi>
2024-05-16 23:32:41 +0300

    ne opne with run print

--- printparams/main.go
+++ printparams/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	args := os.Args[1:]
-	for _, res := range args {
-		fmt.Println(res)
+	arguments := make([]string, len(os.Args))
+	copy(arguments, os.Args)
+
+	for i := 1; i < len(arguments); i++ {
+		argrunes := []rune(arguments[i])
+		for j := 0; j < len(argrunes); j++ {
+			z01.PrintRune(argrunes[j])
+		}
+		z01.PrintRune('\n')
 	}
 }
+
+//danilocangucu/
