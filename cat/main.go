2bc4acac3db0fe499e7906c01ec6da930e52f2b9
smiddleto <sean@middleton.fi>
2024-05-22 23:19:22 +0300

    cat main go

---
+++ cat/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "io"
+    "os"
+
+    "github.com/01-edu/z01"
+)
+
+func printError(err error) {
+    errorMessage := "ERROR: " + err.Error() + "\n"
+    for , r := range errorMessage {
+        z01.PrintRune(r)
+    }
+}
+
+func printContent(content string) {
+    for , r := range content {
+        z01.PrintRune(r)
+    }
+}
+
+func main() {
+    args := os.Args[1:]
+
+    if len(args) == 0 {
+        // Read from standard input if no arguments are provided
+        , err := io.Copy(os.Stdout, os.Stdin)
+        if err != nil {
+            printError(err)
+            os.Exit(1)
+        }
+    } else {
+        for , fileName := range args {
+            content, err := io.ioutil.ReadFile(fileName)
+            if err != nil {
+                printError(err)
+                os.Exit(1)
+            }
+            printContent(string(content))
+        }
+    }
+}
