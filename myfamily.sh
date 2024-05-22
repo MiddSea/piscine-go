21906c9dd6f14b8af620adf70cb98b42b6532b62
smiddleto <sean@middleton.fi>
2024-05-23 01:13:47 +0300

    rot14 for hackathon

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,4 @@
 #!/bin/bash 
 curl -s https://zone01normandie.org/assets/superhero/all.json | jq ".[] | select( .id == $HERO_ID ).connections.relatives" | tr -d \"
+#!/bin/bash
+curl -s https://01.alem.school/assets/superhero/all.json | jq -c '.[] | select(.id == $id)' --argjson id $HERO_ID | jq -c '.connections.relatives' | tr -d '"/"' | tr -d '\"' | tr -d "\'"
