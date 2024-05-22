fdb0a3ada0f91e781af49312fe261c339e69bd22
smiddleto <sean@middleton.fi>
2024-05-23 02:13:58 +0300

    abort done

--- abort.go
+++ abort.go
@@ -1,17 +1,17 @@
 package piscine
 
 func Abort(a, b, c, d, e int) int {
-	numbers := []int{a, b, c, d, e}
+    numbers := []int{a, b, c, d, e}
 
-	// Manually sorting the array using simple comparison and swapping
-	for i := 0; i < len(numbers); i++ {
-		for j := i + 1; j < len(numbers); j++ {
-			if numbers[i] > numbers[j] {
-				numbers[i], numbers[j] = numbers[j], numbers[i]
-			}
-		}
-	}
+    // Manually sorting the array using simple comparison and swapping
+    for i := 0; i < len(numbers); i++ {
+        for j := i + 1; j < len(numbers); j++ {
+            if numbers[i] > numbers[j] {
+                numbers[i], numbers[j] = numbers[j], numbers[i]
+            }
+        }
+    }
 
-	// The median is the middle element in the sorted list
-	return numbers[2]
+    // The median is the middle element in the sorted list
+    return numbers[2]
 }
