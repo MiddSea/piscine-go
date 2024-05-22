fd6fc1cab0bbfb3c8c4fc2aa1430cda217d206be
smiddleto <sean@middleton.fi>
2024-05-23 02:03:19 +0300

    abort.go with sort

---
+++ abort.go
@@ -0,0 +1,15 @@
+package piscine
+
+import "sort"
+
+// Abort returns the median of five int arguments.
+func Abort(a, b, c, d, e int) int {
+    // Create a slice with the five integers
+    numbers := []int{a, b, c, d, e}
+
+    // Sort the slice
+    sort.Ints(numbers)
+
+    // Return the middle element
+    return numbers[2]
+}
