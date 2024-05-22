f8b6bd8404b94fa65f84653751050517d9f4dd01
smiddleto <sean@middleton.fi>
2024-05-23 02:04:07 +0300

    abort remformatted

--- abort.go
+++ abort.go
@@ -4,12 +4,12 @@ import "sort"
 
 // Abort returns the median of five int arguments.
 func Abort(a, b, c, d, e int) int {
-    // Create a slice with the five integers
-    numbers := []int{a, b, c, d, e}
+	// Create a slice with the five integers
+	numbers := []int{a, b, c, d, e}
 
-    // Sort the slice
-    sort.Ints(numbers)
+	// Sort the slice
+	sort.Ints(numbers)
 
-    // Return the middle element
-    return numbers[2]
+	// Return the middle element
+	return numbers[2]
 }
