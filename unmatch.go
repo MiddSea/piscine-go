5e6625358d12acca1edce18d64ba182c8192e5c4
smiddleto <sean@middleton.fi>
2024-05-23 22:00:24 +0300

    asdfsfd

--- unmatch.go
+++ unmatch.go
@@ -2,20 +2,20 @@
 package piscine
 
 func Unmatch(a []int) int {
-    countMap := make(map[int]int)
+	countMap := make(map[int]int)
 
-    // Count occurrences of each element
-    for _, num := range a {
-        countMap[num]++
-    }
+	// Count occurrences of each element
+	for _, num := range a {
+		countMap[num]++
+	}
 
-    // Find the element with an odd count
-    for num, count := range countMap {
-        if count%2 != 0 {
-            return num
-        }
-    }
+	// Find the element with an odd count
+	for num, count := range countMap {
+		if count%2 != 0 {
+			return num
+		}
+	}
 
-    // If all elements have pairs, return -1
-    return -1
+	// If all elements have pairs, return -1
+	return -1
 }
