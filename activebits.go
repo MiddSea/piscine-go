a0cdb9758c945ab077ccfb3b6f51bee93aea1453
smiddleto <sean@middleton.fi>
2024-05-23 16:14:56 +0300

    activebits.go ready

---
+++ activebits.go
@@ -0,0 +1,40 @@
+package piscine
+
+// Write a function, ActiveBits, that returns the number of active bits (bits with the value 1) in the binary representation of an integer number.
+
+// Expected function
+// func ActiveBits(n int) int {
+
+// }
+// Usage
+// Here is a possible program to test your function :
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"piscine"
+// )
+
+// func main() {
+// 	fmt.Println(piscine.ActiveBits(7))
+// }
+// And its output :
+
+// $ go run .
+// 3
+// $
+// Something is wrong ? Submit an issue or even better propose a change !
+
+// piscine/active_bits.go
+package piscine
+
+// ActiveBits returns the number of active bits (bits with the value 1) in the binary representation of an integer.
+func ActiveBits(n int) int {
+	count := 0
+	for n != 0 {
+		count += n & 1 // Check if the least significant bit is 1
+		n >>= 1        // Right shift to check the next bit
+	}
+	return count
+}
