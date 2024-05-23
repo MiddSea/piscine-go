9006ceec669ab36a190c31e3bdd806ef89845494
smiddleto <sean@middleton.fi>
2024-05-23 12:43:05 +0300

    Fix The Main, hack#7

--- fixthemain/main.go
+++ fixthemain/main.go
@@ -6,55 +6,55 @@ import (
 
 // Defining the Door struct
 type Door struct {
-    state int
+	state int
 }
 
 // Defining the constants for door states
 const (
-    OPEN  = 1
-    CLOSE = 0
+	OPEN  = 1
+	CLOSE = 0
 )
 
 func PrintStr(s string) {
-    for _, r := range s {
-        z01.PrintRune(r)
-    }
-    z01.PrintRune('\n')
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
 }
 
 func CloseDoor(ptrDoor Door) bool {
-    PrintStr("Door Closing...")
-    ptrDoor.state = CLOSE
-    return true
+	PrintStr("Door Closing...")
+	ptrDoor.state = CLOSE
+	return true
 }
 
 func IsDoorOpen(doorDoor) bool {
-    PrintStr("is the Door opened ?")
-    return door.state == OPEN
+	PrintStr("is the Door opened ?")
+	return door.state == OPEN
 }
 
 func IsDoorClose(ptrDoor Door) bool {
-    PrintStr("is the Door closed ?")
-    return ptrDoor.state == CLOSE
+	PrintStr("is the Door closed ?")
+	return ptrDoor.state == CLOSE
 }
 
 func OpenDoor(ptrDoorDoor) bool {
-    PrintStr("Door Opening...")
-    ptrDoor.state = OPEN
-    return true
+	PrintStr("Door Opening...")
+	ptrDoor.state = OPEN
+	return true
 }
 
 func main() {
-    door := &Door{}
+	door := &Door{}
 
-    OpenDoor(door)
-    if IsDoorClose(door) {
-        OpenDoor(door)
-    }
-    if IsDoorOpen(door) {
-        CloseDoor(door)
-    }
-    if door.state == OPEN {
-        CloseDoor(door)
-    }
+	OpenDoor(door)
+	if IsDoorClose(door) {
+		OpenDoor(door)
+	}
+	if IsDoorOpen(door) {
+		CloseDoor(door)
+	}
+	if door.state == OPEN {
+		CloseDoor(door)
+	}
 }
