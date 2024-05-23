6606be3a747ff2d59356d1d99b2fabc12b6778ec
smiddleto <sean@middleton.fi>
2024-05-23 16:16:20 +0300

    activebits.go

--- activebits.go
+++ activebits.go
@@ -1,4 +1,15 @@
+// piscine/active_bits.go
 package piscine
+
+// ActiveBits returns the number of active bits (bits with the value 1) in the binary representation of an integer.
+func ActiveBits(n int) int {
+	count := 0
+	for n != 0 {
+		count += n & 1 // Check if the least significant bit is 1
+		n >>= 1        // Right shift to check the next bit
+	}
+	return count
+}
 
 // Write a function, ActiveBits, that returns the number of active bits (bits with the value 1) in the binary representation of an integer number.
 
@@ -25,16 +36,3 @@ package piscine
 // 3
 // $
 // Something is wrong ? Submit an issue or even better propose a change !
-
-// piscine/active_bits.go
-package piscine
-
-// ActiveBits returns the number of active bits (bits with the value 1) in the binary representation of an integer.
-func ActiveBits(n int) int {
-	count := 0
-	for n != 0 {
-		count += n & 1 // Check if the least significant bit is 1
-		n >>= 1        // Right shift to check the next bit
-	}
-	return count
-}
