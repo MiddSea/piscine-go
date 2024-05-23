3621187b4ccd298db89eb1ac4c58167c55e73d8c
smiddleto <sean@middleton.fi>
2024-05-23 14:35:14 +0300

    Compact, hack#8

--- compact.go
+++ compact.go
@@ -1,17 +1,17 @@
 package piscine
 
 func Compact(ptr *[]string) int {
-    slice := *ptr
-    compactSlice := make([]string, 0, len(slice)) // make
-    count := 0
+	slice := *ptr
+	compactSlice := make([]string, 0, len(slice)) // make
+	count := 0
 
-    for _, v := range slice {
-        if v != "" {
-            compactSlice = append(compactSlice, v)
-            count++
-        }
-    }
+	for _, v := range slice {
+		if v != "" {
+			compactSlice = append(compactSlice, v)
+			count++
+		}
+	}
 
-    *ptr = compactSlice
-    return count
+	*ptr = compactSlice
+	return count
 }
