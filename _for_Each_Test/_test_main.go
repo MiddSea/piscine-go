43cec496f4c7d089d5ba6ca912f15a4abf4976a9
smiddleto <sean@middleton.fi>
2024-05-26 09:38:49 +0300

    brace added to end of program

--- _for_Each_Test/_test_main.go
+++ _for_Each_Test/_test_main.go
@@ -1,17 +1,15 @@
-//Usage
-//Here is a possible program to test your function :
-
+// Usage
+// Here is a possible program to test your function :
 package main
 
 import "piscine"
-
-
 
 func main() {
 	a := []int{1, 2, 3, 4, 5, 6}
 	piscine.ForEach(piscine.PrintNbr, a)
 }
-//And its output :
+
+// And its output :
 
 //$ go run .
 //123456
